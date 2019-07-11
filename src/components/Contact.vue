06bf81e386c1937b5d274243c62498e7288a30bb
Dan Addison <danaddisoncreative@gmail.com>
2019-07-11 16:23:27 +0100

    Flesh out the Netlify form stuff

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,33 +1,76 @@
 <template>
   <section class="resume-section" id="contact">
-      <h2>Contact</h2>
-      <form 
-      netlify
+    <h2>Contact</h2>
+
+    <form 
       name="contact"
-      method="POST"
+      method="post"
+      v-on:submit.prevent="handleSubmit"
+      action="/success/"
       data-netlify="true"
-      netlify-honeypot="bot-field">
-        <p class="hidden">
-          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-        </p>
-        <p>
-          <label>Your Name: </label>
-          <input type="text" name="name" />   
-        </p>
-        <p>
-          <label>Your Email: </label>
-          <input type="email" name="email" />
-        </p>
-        <p>
-          <label>Message: </label>
-          <textarea name="message"></textarea>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <p hidden>
+        <label>
+          Don’t fill this out: <input name="bot-field" />
+        </label>
+      </p>
+      
+      <div class="sender-info">
+        <div>
+          <label for="name" class="label" >Your name</label>
+          <input type="text" name="name" v-model="formData.name" />
+        </div>
+        <div>
+          <label for="email">Your email</label>
+          <input type="email" name="email" v-model="formData.email" />
+        </div>
+      </div>
+
+      <div class="message-wrapper">
+        <label for="message">Message</label>
+        <textarea name="message" v-model="formData.message"></textarea>
+      </div>
+
+      <button type="submit">Send</button>
+    </form>
+
   </section>
 </template>
+
+<script>
+
+  export default {
+
+    data() {
+      return {
+        formData: {},
+      }
+    },
+
+    methods: {
+      encode(data) {
+        return Object.keys(data)
+          .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+          .join('&')
+      },
+      handleSubmit(e) {
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: this.encode({
+            'form-name': e.target.getAttribute('name'),
+            ...this.formData,
+          }),
+        })
+        .then(() => this.$router.push('/success'))
+        .catch(error => alert(error))
+      }
+    }
+  }
+
+</script>
 
 <style lang="scss" scoped>
 form {
---
+++ src/pages/Success.vue
@@ -0,0 +1,8 @@
+<template>
+  <Layout>
+    <div class="container">
+      <h1>Boom shake shake shake the room!</h1>
+      <p>Your form submission worked.</p>
+    </div>
+  </Layout>
+</template>
