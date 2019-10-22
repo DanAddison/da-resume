7d1fa53adc737aa1ed79905ad7c0f3b9e1283790
Dan Addison <danaddisoncreative@gmail.com>
2019-10-22 09:22:08 +0100

    Tweak portfolio to reflect new tech stack on Ellis

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,136 +1,199 @@
 <template>
   <section class="resume-section" id="portfolio">
-      <h2>Portfolio</h2>
-      
-      <div class="resume-item">
+    <h2>Portfolio</h2>
 
-        <div class="portfolio-item">
-          <g-image
-            alt="jm-woodcraft.com screenshot"
-            src="../assets/images/portfolio/JM-Woodcraft.jpg"
-            height="312"
-            width="500"
-            fit="cover"
-          />
+    <div class="resume-item">
+      <div class="portfolio-item">
+        <g-image
+          alt="jm-woodcraft.com screenshot"
+          src="../assets/images/portfolio/JM-Woodcraft.jpg"
+          height="312"
+          width="500"
+          fit="cover"
+        />
 
-          <div class="portfolio-item__overlay">
-            <h1>jm-woodcraft.com</h1>
-            <p class="subtitle">Bespoke WordPress site for a carpenter.</p>
-            <p class="tech-stack"><span>Tech stack: </span>WordPress, Sass, jQuery, Gulp</p>
-            <div class="links">
-              <p><a href="https://jm-woodcraft.com" target="_blank">Visit site<font-awesome :icon="['fas', 'external-link-alt']"/></a></p>
-              <p><a href="https://github.com/DanAddison/JM-Woodcraft" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-            </div>
+        <div class="portfolio-item__overlay">
+          <h1>jm-woodcraft.com</h1>
+          <p class="subtitle">Bespoke WordPress site for a carpenter.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> WordPress, Sass, jQuery, Gulp
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://jm-woodcraft.com" target="_blank">
+                Visit site
+                <font-awesome :icon="['fas', 'external-link-alt']" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/DanAddison/JM-Woodcraft" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
           </div>
         </div>
-
-        <div class="portfolio-item">
-            <g-image
-              alt="johnhosking.com screenshot"
-              src="../assets/images/portfolio/John-Hosking.jpg"
-              class="img-profile"
-              height="312"
-              width="500"
-              fit="cover"
-            />
-
-            <div class="portfolio-item__overlay">
-              <h1>johnhosking.com</h1>
-              <p class="subtitle">A portfolio website for an artist.</p>
-              <p class="tech-stack"><span>Tech stack: </span>Sass, Nunjucks, jQuery, Gulp</p>
-              <div class="links">
-                <p><a href="https://johnhosking.com" target="_blank">Visit site<font-awesome :icon="['fas', 'external-link-alt']"/></a></p>
-                <p><a href="https://github.com/DanAddison/John-Hosking" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-              </div>
-            </div>
-        </div>
-
-        <div class="portfolio-item">
-            <g-image
-              alt="dan4ruby.com screenshot"
-              src="../assets/images/portfolio/Dan4Ruby.jpg"
-              class="img-profile"
-              height="312"
-              width="500"
-              fit="cover"
-            />
-            <div class="portfolio-item__overlay">
-              <h1>dan4ruby.com</h1>
-              <p class="subtitle">Wedding event information website.</p>
-              <p class="tech-stack"><span>Tech stack: </span>Sass, Nunjucks, jQuery, Gulp</p>
-              <div class="links">
-                <p><a href="https://dan4ruby.com" target="_blank">Visit site<font-awesome :icon="['fas', 'external-link-alt']"/></a></p>
-                <p><a href="https://github.com/DanAddison/dan4ruby" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-              </div>
-            </div>
-        </div>
-
-        <div class="portfolio-item">
-          <g-image
-            alt="ellisbolton.com screenshot"
-            src="../assets/images/portfolio/Ellis-Bolton.jpg"
-              class="img-profile"
-              height="312"
-              width="500"
-            fit="cover"
-          />
-          <div class="portfolio-item__overlay">
-              <h1>ellisbolton.com</h1>
-              <p class="subtitle">A portfolio website for an illustrator.</p>
-              <p class="tech-stack"><span>Tech stack: </span>WordPress, Sass, jQuery, Gulp</p>
-              <div class="links">
-                <p><a href="https://ellisbolton.com" target="_blank">Visit site<font-awesome :icon="['fas', 'external-link-alt']"/></a></p>
-                <p><a href="https://github.com/DanAddison/ellis-bolton-wordpress" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-              </div>
-            </div>
-        </div>
-
-        <div class="portfolio-item">
-          <g-image
-            alt="Pen &amp; Plant website screenshot"
-            src="../assets/images/portfolio/Pen-and-Plant.jpg"
-              class="img-profile"
-              height="312"
-              width="500"
-            fit="cover"
-          />
-          <div class="portfolio-item__overlay">
-              <h1>pen-and-plant.com</h1>
-              <p class="subtitle">An online portfolio and shop for an illustrator.</p>
-              <p class="tech-stack"><span>Tech stack: </span>WordPress, WooCommerce, Sass, jQuery, Gulp</p>
-              <div class="links">
-                <p><a href="https://github.com/DanAddison/Pen-and-Plant" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-              </div>
-            </div>
-        </div>
-
-        <div class="portfolio-item">
-            <g-image
-              alt="daniel-addison-lyrics-archive screenshot"
-              src="../assets/images/portfolio/Lyrics-Archive.jpg"
-              class="img-profile"
-              height="312"
-              width="500"
-              fit="cover"
-            />
-            <div class="portfolio-item__overlay">
-              <h1>lyrics-archive.com</h1>
-              <p class="subtitle">A documentation website for a songwriter.</p>
-              <p class="tech-stack"><span>Tech stack: </span>Markdown, Javascript, Docsify</p>
-              <div class="links">
-                <p><a href="https://daniel-addison-lyrics-archive.netlify.com/#/" target="_blank">Visit site<font-awesome :icon="['fas', 'external-link-alt']"/></a></p>
-                <p><a href="https://github.com/DanAddison/Songwriting" target="_blank">View code<font-awesome :icon="['fab', 'github']"/></a></p>
-              </div>
-            </div>
-        </div>
-
       </div>
-      
+
+      <div class="portfolio-item">
+        <g-image
+          alt="johnhosking.com screenshot"
+          src="../assets/images/portfolio/John-Hosking.jpg"
+          class="img-profile"
+          height="312"
+          width="500"
+          fit="cover"
+        />
+
+        <div class="portfolio-item__overlay">
+          <h1>johnhosking.com</h1>
+          <p class="subtitle">A portfolio website for an artist.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> Sass, Nunjucks, jQuery, Gulp
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://johnhosking.com" target="_blank">
+                Visit site
+                <font-awesome :icon="['fas', 'external-link-alt']" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/DanAddison/John-Hosking" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="portfolio-item">
+        <g-image
+          alt="dan4ruby.com screenshot"
+          src="../assets/images/portfolio/Dan4Ruby.jpg"
+          class="img-profile"
+          height="312"
+          width="500"
+          fit="cover"
+        />
+        <div class="portfolio-item__overlay">
+          <h1>dan4ruby.com</h1>
+          <p class="subtitle">Wedding event information website.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> Sass, Nunjucks, jQuery, Gulp
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://dan4ruby.com" target="_blank">
+                Visit site
+                <font-awesome :icon="['fas', 'external-link-alt']" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/DanAddison/dan4ruby" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="portfolio-item">
+        <g-image
+          alt="ellisbolton.com screenshot"
+          src="../assets/images/portfolio/Ellis-Bolton.jpg"
+          class="img-profile"
+          height="312"
+          width="500"
+          fit="cover"
+        />
+        <div class="portfolio-item__overlay">
+          <h1>ellisbolton.com</h1>
+          <p class="subtitle">A portfolio website for an illustrator.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> Gatsby, React, Rebass, Styled Components
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://ellisbolton.com" target="_blank">
+                Visit site
+                <font-awesome :icon="['fas', 'external-link-alt']" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/DanAddison/ellis-bolton-wordpress" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="portfolio-item">
+        <g-image
+          alt="Pen &amp; Plant website screenshot"
+          src="../assets/images/portfolio/Pen-and-Plant.jpg"
+          class="img-profile"
+          height="312"
+          width="500"
+          fit="cover"
+        />
+        <div class="portfolio-item__overlay">
+          <h1>pen-and-plant.com</h1>
+          <p class="subtitle">An online portfolio and shop for an illustrator.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> WordPress, WooCommerce, Sass, Gulp
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://github.com/DanAddison/Pen-and-Plant" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="portfolio-item">
+        <g-image
+          alt="daniel-addison-lyrics-archive screenshot"
+          src="../assets/images/portfolio/Lyrics-Archive.jpg"
+          class="img-profile"
+          height="312"
+          width="500"
+          fit="cover"
+        />
+        <div class="portfolio-item__overlay">
+          <h1>lyrics-archive.com</h1>
+          <p class="subtitle">A documentation website for a songwriter.</p>
+          <p class="tech-stack">
+            <span>Tech stack:</span> Markdown, Javascript, Docsify
+          </p>
+          <div class="links">
+            <p>
+              <a href="https://daniel-addison-lyrics-archive.netlify.com/#/" target="_blank">
+                Visit site
+                <font-awesome :icon="['fas', 'external-link-alt']" />
+              </a>
+            </p>
+            <p>
+              <a href="https://github.com/DanAddison/Songwriting" target="_blank">
+                View code
+                <font-awesome :icon="['fab', 'github']" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-
 </script>
 
 
@@ -150,9 +213,8 @@ section.resume-section {
   min-width: 300px;
   overflow: hidden;
   margin-bottom: 2rem;
-  box-shadow: 0px 0px 11px -6px rgba(0,0,0,0.75);
-  transition: .2s;
-
+  box-shadow: 0px 0px 11px -6px rgba(0, 0, 0, 0.75);
+  transition: 0.2s;
 
   @media (min-width: 600px) {
     max-width: 47%;
@@ -169,7 +231,7 @@ section.resume-section {
   &:hover,
   &:focus {
     transform: scale(1.07);
-    box-shadow: 0px 0px 11px -2px rgba(0,0,0,0.5);
+    box-shadow: 0px 0px 11px -2px rgba(0, 0, 0, 0.5);
   }
 
   img {
@@ -192,7 +254,7 @@ section.resume-section {
   flex-grow: 1;
   justify-content: center;
   background-color: $primary-overlay;
-  transition: .3s;
+  transition: 0.3s;
   opacity: 0;
 
   &:hover,
@@ -214,14 +276,14 @@ section.resume-section {
 
   .subtitle {
     font-weight: bold;
-    margin: .5em 0 .7em;
+    margin: 0.5em 0 0.7em;
   }
 
   .tech-stack {
     color: $copy-light;
-    font-size: .9rem;
+    font-size: 0.9rem;
   }
-    
+
   .links {
     position: absolute;
     bottom: 1rem;
@@ -232,12 +294,12 @@ section.resume-section {
 
     p {
       flex-grow: 1;
-      margin-bottom: .5em;
+      margin-bottom: 0.5em;
       margin-top: 0;
       color: $primary;
-      
+
       svg {
-        padding-left: .3em;
+        padding-left: 0.3em;
       }
 
       a {
@@ -251,5 +313,4 @@ section.resume-section {
     }
   }
 }
-
 </style>
